0bc0d33fb9ca4a10a301452bd1074bcbac7d1398
Emily Pan <mpan@fb.com>
2017-06-21 17:09:46 -0700

    added edit item feature

--- app/src/main/java/com/codepath/simpletodo/MainActivity.java
+++ app/src/main/java/com/codepath/simpletodo/MainActivity.java
@@ -1,5 +1,6 @@
 package com.codepath.simpletodo;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -18,6 +19,10 @@ import java.nio.charset.Charset;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    public final static int EDIT_REQUEST_CODE = 20;
+    public final static String ITEM_TEXT = "itemText";
+    public final static String ITEM_POSITION = "itemPosition";
 
     ArrayList<String> items;
     ArrayAdapter<String> itAdapter;
@@ -61,8 +66,30 @@ public class MainActivity extends AppCompatActivity {
                 Log.i("MainActivity", "Removed item " + position);
                 return true;
             }
-
         });
+        lvItems.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent i = new Intent(MainActivity.this, EditItemActivity.class);
+                i.putExtra(ITEM_TEXT, items.get(position));
+                i.putExtra(ITEM_POSITION, position);
+
+                startActivityForResult(i, EDIT_REQUEST_CODE);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK && requestCode == EDIT_REQUEST_CODE) {
+            String updated = data.getExtras().getString(ITEM_TEXT);
+            int pos = data.getExtras().getInt(ITEM_POSITION);
+            items.set(pos, updated);
+            itAdapter.notifyDataSetChanged();
+            writeItems();
+            Toast.makeText(this, "Item updated successfully", Toast.LENGTH_SHORT).show();
+        }
     }
 
     private File getDataFile() {
